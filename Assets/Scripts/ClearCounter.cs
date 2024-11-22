0634f5eef104ed95dbed76e232eb2dd4bc9a01cb
Mehmet Berkay Coruk <beqostore@gmail.com>
2024-11-22 18:09:26 +0300

    Pickup and Drop, Counter Logic Complete
    Container and Clear counter variants added
    
    Pickup and Drop fully functional

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -6,6 +6,31 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        // Drop and Pickup items
+        if (HasKitchenObject())
+        {
+            // There is a KitchenObject on the counter
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+            }
+            else
+            {
+                // Player has nothing
+                GetKitchenObject().SetKitchenObjectParent(player); // Give to player
+            }
+        }
+        else
+        {
+            // there is no KitchenObject on the counter
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this); // Take from player
+            }
+            else
+            {
+                // Player has nothing
+            }
+        }
     }
 }
