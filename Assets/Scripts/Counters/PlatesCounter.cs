f415c526a2ddc70ec73c536e57b4354fb47bbbf2
Mehmet Berkay Coruk <beqostore@gmail.com>
2024-11-25 17:10:23 +0300

    PlateCounter implemented

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
+    private float _spawnPlateTimer;
+    private float _spawnPlateTimerMax = 4f;
+    private int _platesSpawnedAmount;
+    private int _platesSpawnedAmountMax = 4;
+
+    private void Update()
+    {
+        _spawnPlateTimer += Time.deltaTime;
+        if (_spawnPlateTimer > _spawnPlateTimerMax)
+        {
+            _spawnPlateTimer = 0;
+
+            if (_platesSpawnedAmount < _platesSpawnedAmountMax)
+            {
+                _platesSpawnedAmount++;
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            // Player is empty handed
+            if (_platesSpawnedAmount > 0)
+            {
+                // There is at least 1 plate
+                _platesSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
